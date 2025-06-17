dda88a8bb0ffc8ff1f4ad204bcf49fa796be4acb
AxiosLeo <13862149+AxiosLeo@users.noreply.github.com>
2025-06-17 15:20:55 +0800

    chore(ecosystem): comment out unused logging options
    Removed several logging options from the webhook-consumer
    configuration in ecosystem.config.js to clean up the code.
    These options are currently not in use and have been commented
    out for clarity and maintainability.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -47,15 +47,15 @@ module.exports = {
       error_file: './logs/webhook-consumer-error.log',
       out_file: './logs/webhook-consumer-out.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
-      merge_logs: true,
-      log_type: 'json',
-      max_restarts: 10,
-      restart_delay: 4000,
-      exp_backoff_restart_delay: 100,
-      log_rotate_interval: '0 0 * * *',
-      log_rotate_max_size: '100M',
-      log_rotate_keep: 7,
-      log_rotate_compress: true
+      // merge_logs: true,
+      // log_type: 'json',
+      // max_restarts: 10,
+      // restart_delay: 4000,
+      // exp_backoff_restart_delay: 100,
+      // log_rotate_interval: '0 0 * * *',
+      // log_rotate_max_size: '100M',
+      // log_rotate_keep: 7,
+      // log_rotate_compress: true
     }
   ]
 };
