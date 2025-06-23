7b76abfdfe8f43444125cae2977190007df07a6f
axiosleo <axiosleo@foxmail.com>
2025-06-23 10:25:56 +0800

    refactor(api): enhance API routing and event handling for Coding and GitHub
    - Refactored the API routes to improve organization and clarity.
    - Introduced separate routes for handling events from Coding and GitHub platforms.
    - Implemented signature verification for GitHub webhooks to enhance security.
    - Streamlined event processing logic for both platforms, ensuring better handling of incoming requests and payloads.
    - Removed commented-out code to improve readability and maintainability.

--- src/api.js
+++ src/api.js
@@ -1,56 +1,91 @@
 'use strict';
 
 const { Router, error, success } = require('@axiosleo/koapp');
-const { debug } = require('@axiosleo/cli-tool');
 const { sendTask } = require('./task');
 const CodingController = require('./controllers/coding');
 const GithubController = require('./controllers/github');
+const config = require('../config');
 
-const auth = async (context) => {
-  // const ctx = context.koa;
-  // const userAgent = ctx.request.header['user-agent'];
-  // if (userAgent !== 'Coding.net Hook') {
-  //   error(403, 'Unauthorized');
-  // }
-};
-
-const root = new Router('', {
-  middlewares: [auth]
-});
+const root = new Router();
 
 root.new('/***', {
   method: 'any',
-  handlers: [async (context) => {
-    debug.log(context.body, context.router);
-    success({ router: context.router });
+  handlers: [async () => {
+    error(404, 'Not Found');
   }]
 });
 
-root.post('/{:platform}/{:team}/{:project}', async (context) => {
-  const { platform, team, project } = context.params;
-  const body = context.body;
-  let task = null;
-  if (platform === 'github') {
-    const controller = new GithubController();
-    task = await controller.receiveEvent(team, project, JSON.parse(body.payload));
-  } else if (platform === 'coding') {
-    const controller = new CodingController();
-    task = await controller.receiveEvent(team, project, body);
-  } else {
-    error(400, 'Unknown platform');
-  }
-  if (!task) {
-    success({ event: 'unknown' });
-  }
-  await sendTask(task);
-  success({ event: task.event });
-}, {
-  params: {
-    rules: {
-      team: 'required|string',
-      project: 'required|string',
+root.new('/coding/{:team}/{:project}', {
+  method: 'post',
+  handlers: [
+    /**
+     * @param {import('@axiosleo/koapp').KoaContext} context
+     */
+    async (context) => {
+      const { team, project } = context.params;
+      const body = context.body;
+
+      let task = null;
+      const controller = new CodingController();
+      task = await controller.receiveEvent(team, project, body);
+      if (!task) {
+        success({ event: 'unknown' });
+      }
+      await sendTask(task);
+      success({ event: task.event });
+    }, {
+      params: {
+        rules: {
+          team: 'required|string',
+          project: 'required|string',
+        }
+      }
     }
-  }
+  ]
+});
+
+root.new('/github/{:team}/{:project}', {
+  method: 'post',
+
+  handlers: [
+    /**
+     * @param {import('@axiosleo/koapp').KoaContext} context
+     */
+    async (context) => {
+      const controller = new GithubController();
+      if (config.github.webhook_secret) {
+        const signature = context.headers['x-hub-signature-256'];
+        if (!signature) {
+          error(403, 'Unauthorized');
+        }
+
+        // 获取原始请求体内容
+        const rawBody = context.koa.request.rawBody || context.koa.req.rawBody || '';
+
+        const isValid = await controller.verifySignature(config.github.webhook_secret, signature, rawBody);
+        if (!isValid) {
+          error(403, 'Unauthorized');
+        }
+      }
+      const { team, project } = context.params;
+      const body = context.body;
+      let task = null;
+
+      task = await controller.receiveEvent(team, project, JSON.parse(body.payload));
+      if (!task) {
+        success({ event: 'unknown' });
+      }
+      await sendTask(task);
+      success({ event: task.event });
+    }, {
+      params: {
+        rules: {
+          team: 'required|string',
+          project: 'required|string',
+        }
+      }
+    }
+  ]
 });
 
 module.exports = root;
